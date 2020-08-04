888026ff8bbb63b08223b5b74fb6a85974ebda7f
jesusfranck09 <d93409@gmail.com>
2020-08-04 11:42:10 -0500

    first commit

---
+++ src/component/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+        files:[]
+     }
+    }
+
+    
+
+  showFile = async (e) => {
+    e.preventDefault()
+   let array=[]
+    
+    let splitTextfilter; 
+  
+
+    const reader = new FileReader()
+    reader.onload = async (e) => { 
+      const text = (e.target.result)
+      this.setState({files:text})
+    // console.log("que hay en text", text)
+      
+    var splitText=text.split('*')
+
+     
+    // console.log("array",array)
+
+    // console.log("splitText",splitText)
+  
+     //splitTextfilter = splitText.filter(function(filtro){
+      //console.log("filtro ", filtro)
+      //return filtro
+    //  })
+ 
+    // 
+    let array=[]
+     array.push(splitText)
+     let array2=[]
+    array[0].map(rows=>{
+     let spelit =  rows.split('-')
+        array.push(spelit[0])
+        // console.log(array)
+        let array3=[]
+         for(let i =2 ; i<=array.length;i++){
+         array3.push(array[i] + 10)
+          console.log("array3" , spelit)
+        }
+
+    })
+
+    //  array[0].filter(function(hero){
+    //   console.log("hero" , hero)
+    //  })
+     //  console.log("nuevo filter", splitText1)
+     // console.log("this,state" , this.state.files)
+      // console.log("string", text.split('*') )
+    };
+    reader.readAsText(e.target.files[0])
+  } 
+//_________________________
+
+
+
+
+  render = () => {
+
+    return (
+    <div>
+      <input type="file"  accept =".txt"  onChange={(e) => this.showFile(e)} />
+    </div>
+  // <h1>hola</h1>
+    )
+  }
+}
+
+export default Index;
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+import {BrowserRouter as Router , Switch ,Route} from 'react-router-dom'
+
+import './App.css';
+import index from './component/index'
+
+class App extends Component{
+
+render(){
+  return(
+
+    <Router>
+    <Switch>
+    <main>
+    
+        <Route exact path = "/" component ={index}/>   
+    </main>
+    </Switch>          
+    </Router>
+  )
+}
+}
+export default App
